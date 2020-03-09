690940131a859d1f45788da4bd80cfb685037cb1
Kyle Brown <kylebrw@gmail.com>
2020-03-09 01:07:14 -0400

    version 2

--- app-setup.sh
+++ app-setup.sh
@@ -1,42 +1,44 @@
 #! /bin/bash
-upgrade_everyting
+./upgrade-everything.sh
 
 # Firefox Setup
-firefox about:preferences
+firefox about:preferences &> /dev/null &
+firefox https://addons.mozilla.org/en-US/firefox/addon/ublock-origin &> /dev/null &
+firefox https://addons.mozilla.org/en-US/firefox/addon/disconnect &> /dev/null &
+firefox https://www.dashlane.com/download &> /dev/null &
 read -n 1 -p "Disable 'Ctrl+Tab cycles through tabs in recently used'"
 read -n 1 -p "Install Ublock"
 read -n 1 -p "Install Disconnect"
-firefox https://www.dashlane.com/download &
 read -n 1 -p "Install Dashlane"
 
 # PureVPN
-read -n 1 -p "Install PureVPN"
+# read -n 1 -p "Install PureVPN"
 
 # Snap Apps
 sudo apt-get install snapd -y 
 
 sudo snap install code --classic
-echo "Visual Studio Code IDE Installed"
+printf "Visual Studio Code IDE Installed\n"
 sudo snap install discord
-echo "Discord Messaging App Installed"
+printf "Discord Messaging App Installed\n"
 sudo snap install spotify
-echo "Spotify Music App Installed"
+printf "Spotify Music App Installed\n"
 sudo snap install insomnia
-echo "Insomnia REST Client Installed"
+printf "Insomnia REST Client Installed\n\n"
 
 # Other Apps
 sudo apt-get install vlc -y
-echo "VLC Media Player Installed"
+printf "VLC Media Player Installed\n\n"
 
 # GIT Key Setup - create a new ssh key, open github)
 # firefox https://github.com/settings/keys &
 # cat /dev/zero | ssh-keygen -q -N ""
-# echo -e "\n\n"
+# printf -e "\n\n"
 # cat ~/.ssh/id_rsa.pub
-# echo -e "\n\n"
+# printf -e "\n\n"
 # read -n 1 -p "Copy this as your new key to finish Github setup"
 
-upgrade_everything 
+./upgrade-everything.sh
 
 # Finished
-echo "\n\n\nThat's all folks!"
+printf "\nThat's all folks!\n"
--- os-and-shell.sh
+++ os-and-shell.sh
@@ -1,21 +1,62 @@
 #! /bin/bash
 
-# Powerline (terminal prettiness)
-# Get powerline fonts and then the package
+# UI and Quality of Life Improvements
+
+## Redshift (flux-like night light package)
+## See https://github.com/jonls/redshift
+sudo apt-get install redshift-gtk
+if [ -f "~/.config/redshift.conf" ]
+then
+  printf "Copied Redshift config to ~/.config/redshift.conf\n\n"
+  cp ./redshift.conf ~/.config/redshift.conf
+else
+  printf "Redshift config is already present\n\n"
+fi
+
+## Fix Scrolling Direction
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+printf "Scrolling Direction Fixed\n\n"
+
+# Terminal Setup
+
+## Install Powerline (terminal prettiness)
 sudo apt-get install fonts-powerline -y
 sudo apt-get install powerline -y 
-echo "Powerline Installed"
+printf "Powerline Installed\n\n"
 
-# Fira Code (fav font)
-sudo apt install fonts-firacode -y
+## Install Fira Code (fav font)
+sudo apt-get install fonts-firacode -y
 read -n 1 -p "In terminal preferences select 'Fira Mono Medium'"
-echo "Finished Installing Fonts!"
+printf "Finished Installing Fonts\n\n"
 
-# Add Powerline and upgrade_everything function to shell
-cat ./bashrc-additions.sh >> ~/.bashrc
-echo "Added Powerline to .bashrc"
-echo "Added upgrade_everything to .bashrc"
+## Add Powerline to shell
+if grep -q "POWERLINE-FINGERPRINT" ~/.bashrc;
+then
+  printf "Powerline already present in .bashrc\n\n"
+else
+  cat ./bashrc-addons/powerline.sh >> ~/.bashrc
+  printf "Added Powerline to .bashrc\n\n"
+fi
 
-# Fix Scrolling Direction
-echo "Go to Settings -> Devices -> Mouse & Touchpad"
-read -n 1 -p "Enable `Natural Scrolling - Scrolling moves the content, not the view`"
+## Add upgrade_everything function to shell
+if grep -q "UPGRADE-EVERYTHING-FINGERPRINT" ~/.bashrc;
+then
+  printf "upgrade_everything already present in .bashrc\n\n"
+else
+  cat ./bashrc-addons/upgrade-everything.sh >> ~/.bashrc
+  printf "Added upgrade_everything to .bashrc\n\n"
+fi
+
+printf "OS and Shell setups complete!\n\n"
+
+# Reboot Prompt
+read -p "Would you like to reboot? [Y/n]: " should_reboot
+if [[ $should_reboot == "" || $should_reboot =~ [Yy] ]]
+then
+  printf "Rebooting!\n"
+  reboot
+else
+  printf "Completed, no reboot\n"
+fi
+
+
